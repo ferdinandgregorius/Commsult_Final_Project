e89af3d19d3054284c1b7c6355beb38220b4ee2f
WilliamOEY <WilliamOEY@users.noreply.github.com>
2021-10-21 21:05:57 +0700

    Added WeatherTimer.java and added declaration to MainRunner.java

---
+++ src/main/java/Ferdinand_William_project/WeatherTimer.java
@@ -0,0 +1,20 @@
+package Ferdinand_William_project;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WeatherTimer {
+	int secondsPassed = 0;
+	Timer wTimer = new Timer();
+	TimerTask task = new TimerTask() {
+		public void run() {
+			secondsPassed++;
+			System.out.println("Hours passed: " + secondsPassed);
+		}
+	};
+
+	public void start() {
+		wTimer.scheduleAtFixedRate(task, 1000, 1000);
+	}
+	
+}
